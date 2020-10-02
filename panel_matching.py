b0304fba02a0febc6cb0eb4d375c7bee8c3998d1
Jay Miles <jay.miles@addenbrookes.nhs.uk>
2020-10-02 15:53:04 +0100

    Add ranking capability and excel output

--- panel_matching.py
+++ panel_matching.py
@@ -37,36 +37,33 @@ def create_panelapp_dict():
             panelapp_dictionary[filename[:-4]] = gene_symbols
     return panelapp_dictionary
 
-#SECTION 3: For each gemini panel, which panelapp panel (or combination thereof) best covers those genes?
-#What are the possible solutions?
-#Single-panel solutions 
-#	1. One panelapp panel which is exactly the same as the gemini panel (ideal solution)
-#	2. The shortest panelapp panel which contains the gemini panel plus additional genes (may be a LOT additional genes)
-#   3. The longest panelapp panel which is contained within the gemini panel (may be a LOT of missing genes, and may also have additional genes)
-#Multi-panel solutions:
-#   4. Multiple panelapp panels, which added together exactly replicate the gemini panel
-#   5. Multiple panelapp panels, which added together cover all the relevant genes and some others (may be a LOT additional genes)
-#   6. Multiple panelapp panels, which added together cover some of the relevant genes (may be a LOT of missing genes, and may also have additional genes)
-#'Give up and go to the pub' case:
-#   7. There are no panelapp panels which cover any genes in the gemini panel
+#SECTION 3: For each Gemini panel, which PanelApp panel best covers those genes?
+#The ideal solution is that a PanelApp panel exactly matches the Gemini panel. Unfortunately, this is only true for 3 Gemini panels.
+
+#The approach taken here is to determine, for each Gemini panel and each PanelApp panel:
+#   (a) the % genes in the Gemini panel which the PanelApp panel covers (ideally high)
+#   (b) the % genes in the PanelApp panel which are not in the Gemini panel (surplus genes, ideally low)
+
+#The script performs 2 rankings. Firstly, panels are split into ranks to select for decreasingly high values of (a). Secondly, each rank is ordered by values of (b).
+#The best match is the panel from the highest rank with the lowest value for (b).
 
 def create_mapped_dict():
+    import xlsxwriter
     gemini_dictionary = create_gemini_dict()
     panelapp_dictionary = create_panelapp_dict()
     
-    mapped_dictionary = {} #{'gemini panel name':'best panelapp match'}
+    mapped_dictionary = {}
     for gemini_panel, gemini_genes in gemini_dictionary.items():
-        exact_matches = [] #panelapps which exactly match gemini
-        bigger_panels = {} #panelapps which cover all gemini genes plus others {'panel name':[list of genes in panel, number surplus genes]}
-        shortest_bigger_panel = [] #['panel name', surplus]
-        subpanels = [] #panelapps which are subpanels of gemini
+        best_match = ''
+        rank_values = {}
 
         for panelapp_panel, panelapp_genes in panelapp_dictionary.items():
-            #A: Panelapp panel identical to gemini panel. Gets appended to exact_matches.
+            #If the panelapp panel is identical to gemini panel (best-case scenario), save as best match and break out of sub-loop
             if panelapp_genes == gemini_genes:
-                exact_matches.append(panelapp_panel)
-                continue
+                best_match = panelapp_panel
+                break
         
+            #Identify differences between gemini and panelapp panels
             shared_genes = []
             gemini_only = []
             panelapp_only = []
@@ -80,72 +77,59 @@ def create_mapped_dict():
                 if gene not in gemini_genes:
                     panelapp_only.append(gene)
         
-            #B: Panelapp panel doesn't cover any genes in gemini panel. Move to next panelapp panel.
+            #If the panelapp panel doesn't cover any genes in gemini panel, skip to the next panelapp panel
             if gemini_only == gemini_genes:
                 continue
+
+            #D: Calculate values for ranking
+            pc_coverage = len(shared_genes) / len(gemini_genes) #percentage of gemini genes covered by this panel
+            pc_surplus = len(panelapp_only) / len(panelapp_genes) #surplus genes (those not in gemini panel) as a percentage of panelapp panel length
+            rank_values[panelapp_panel] = [pc_coverage, pc_surplus]
         
-            #C: Panelapp panel covers all genes in gemini panel plus some others. Append [panelapp panel name, number surplus genes] to dictionary for gemini panel.
-            elif gemini_genes in panelapp_genes:
-                surplus = len(panelapp_genes) - len(gemini_genes)
-                bigger_panels[panelapp_panel] = [panelapp_genes, surplus]
-                continue
-        
-            #D: Panelapp panel is subpanel of gemini panel. Add to subpanels list but don't move on yet.
-            elif panelapp_genes in gemini_genes:
-                subpanels.append(panelapp_panel)
+        #If a panel was an exact match, that's the best match.
+        if best_match:
+            mapped_dictionary[gemini_panel] = best_match
+            continue
 
-            #E: If none of the above apply, we have to rank the panels somehow.
-            #This is essentially an optimisation problem with three parts:
-            #   maximise (shared_genes)
-            #   minimise both (gem_only) and (pan_only)
-
-
-
-            #EXIT SUB-LOOP
-        #Generate output for the current gemini panel
-        best_match = ''
-        
-        #If there is an exact match, that's the best match.
-        if exact_matches:
-            best_match = exact_matches
-
-        #If there are any panels which cover all genes in the current gemini panel, the shortest of those is the best match
-        elif bigger_panels:
-            for panel, surplus in bigger_panels.items():
-                if not shortest_bigger_panel:
-                    shortest_bigger_panel = [panel, surplus]
-                elif surplus < shortest_bigger_panel[1]:
-                    shortest_bigger_panel = [panel, surplus]
-            best_match = '{panel} covers the same genes plus {surplus} others'.format(panel=shortest_bigger_panel[0], surplus = shortest_bigger_panel[1])
-
-        #If there are any panels which are subpanels of the gemini panel
-            #FIGURE OUT WHAT TO DO WITH SUBPANELS
-
-        #If there are ranked panels
-            #FIGURE OUT WHAT TO DO WITH RANKED PANELS
+        #Otherwise, panels are ranked for the highest % gemini genes covered AND the fewest surplus.
+        else:
+            list_of_ranks = {}
+            for i in range(6):
+                list_of_ranks[i] = []
+                if i == 1:
+                    for panel, values in rank_values.items():
+                        if values[0] == 1 and 0 <= values[1] <= 0.5: #Rank 1 - panels which cover 100% of gemini genes
+                            if not list_of_ranks[1]:
+                                list_of_ranks[1] = [panel, values[1]]
+                            elif values[1] < list_of_ranks[1][1]:
+                                list_of_ranks[1] = [panel, values[1]]
+                else:
+                    for panel, values in rank_values.items():
+                        if (1-(0.1 * (i-1))) <= values[0] < (1-(0.1 * (i-2))) and 0 <= values[1] <= 0.5: #Ranks 2-6 - range of (% gemini genes covered) decreases by 10% with each rank
+                            if not list_of_ranks[i]:
+                                list_of_ranks[i] = [panel, values[1]]
+                            elif values[1] < list_of_ranks[i][1]:
+                                list_of_ranks[i] = [panel, values[1]]                    
+           
+                if list_of_ranks[i]: #break the ranking loop at the highest occupied rank
+                    best_match = list_of_ranks[i][0]
+                    mapped_dictionary[gemini_panel] = best_match
     
-
-        mapped_dictionary[gemini_panel] = best_match
-        #EXIT MAIN LOOP
+    #Create an excel file showing the mapping of panelapp to gemini panels, including the genes in each panel
+    workbook = xlsxwriter.workbook('mapping_output.xlsx')
+    worksheet = workbook.add_worksheet()
+    row = 0
+    col = 0
+    for key, value in mapped_dictionary.items():
+        worksheet.write(row, col, key)
+        worksheet.write(row, col+1, gemini_dictionary[key])
+        worksheet.write(row, col+2, value)
+        worksheet.write(row, col+3, panelapp_dictionary[key])
+        row += 1
+    
     return mapped_dictionary
-    #EXIT FUNCTION
 
 mapped_dictionary = create_mapped_dict()
-
-#Test 1: print all mapped_dictionary contents
-for key, value in mapped_dictionary.items():
-    if value:
-        print(key, value)
-
-#Test 2: Search in terminal for match to specific panel
-#search_item = input('Which Gemini panel are you interested in? ')
-#print(mapped_dictionary[search_item])
-
-
-
-
-
-
 
 
 # SECTION 4: NOT PART OF THE PROGRAM, JUST FOR INFO--------------------
